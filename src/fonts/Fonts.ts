b28e5557ce2e05816119e16dbeed98a28a675e5c
Aleksandr <a.teslin90@gmail.com>
2023-06-10 15:40:28 +0300

    "init: start project"

---
+++ src/fonts/Fonts.ts
@@ -0,0 +1,11 @@
+import { Noto_Sans, Caveat_Brush } from "next/font/google";
+
+export const noto_sans = Noto_Sans({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+});
+
+export const caveat_brush = Caveat_Brush({
+  subsets: ["latin"],
+  weight: "400",
+});
